8db3f31df9a7ae9fb230aa340e410f15e78e7268
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-07-03 15:44:12 -0500

    refactor: remove commented sections in post.route.js for cleaner code

--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -12,12 +12,10 @@ import upload from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
-// public routes
 router.get("/", getPosts);
 router.get("/:postId", getPost);
 router.get("/user/:username", getUserPosts);
 
-// protected proteced
 router.post("/", protectRoute, upload.single("image"), createPost);
 router.post("/:postId/like", protectRoute, likePost);
 router.delete("/:postId", protectRoute, deletePost);
