79695283dba6ac567102989dd534eeb839326d5e
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-07-03 15:43:59 -0500

    refactor: remove unnecessary comments in user.route.js

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -10,10 +10,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// public route
 router.get("/profile/:username", getUserProfile);
 
-// protected routes
 router.post("/sync", protectRoute, syncUser);
 router.get("/me", protectRoute, getCurrentUser);
 router.put("/profile", protectRoute, updateProfile);
